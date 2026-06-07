1f2d14b6c11903c9d25f5fc6241ed43f3e47940f
jackelchen <jackelchen007@gmail.com>
2026-06-07 17:47:27 +0800

    Modify: 增加偏移量适配图标位置:大世界-码头地图-作战总览

--- module/os_handler/enemy_searching.py
+++ module/os_handler/enemy_searching.py
@@ -8,7 +8,7 @@ class EnemySearchingHandler(EnemySearchingHandler_):
     def is_in_map(self):
         if IN_MAP.match_luma(self.device.image, offset=(200, 5)):
             return True
-        if self.match_template_color(MAP_GOTO_GLOBE_FOG, offset=(5, 5)):
+        if self.match_template_color(MAP_GOTO_GLOBE_FOG, offset=(200, 5)):
             return True
 
         return False
